f1adf51de84b903b570c96dc65a693da7b2256a5
Ian Webster <ian.webster@discordapp.com>
2020-12-02 14:51:44 -0500

    make shard entrypoint better

--- bot/shard_entrypoint.js
+++ bot/shard_entrypoint.js
@@ -1,5 +1,11 @@
+const path = require('path');
+
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: process.env.BOT_TOKEN });
+
+const botPath = path.join(__dirname, './bot.js');
+const manager = new ShardingManager(botPath, {
+  token: process.env.BOT_TOKEN,
+});
 
 manager.on('shardCreate', (shard) => console.log(`Launched shard ${shard.id}`));
 
